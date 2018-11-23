5ff13fc03c043eda2ce084ff4cdacd2c44e5ce9b
Leo Rønsholt Mikkelsen <leox0204@edu.nextkbh.dk>
2018-11-23 20:39:59 +0100

    Changed the method for getting the mac address of the computer, still doesn't work though...

--- Formelsamling/Authentication/MacAddress.cs
+++ Formelsamling/Authentication/MacAddress.cs
@@ -11,8 +11,50 @@ namespace Formelsamling.Authentication
 {
     class GetMacAddress
     {
-        
-        public static NetworkInterface[] ShowNetworkInterfaces()
+        public static PhysicalAddress ShowNetworkInterfaces()
+        {
+            IPGlobalProperties computerProperties = IPGlobalProperties.GetIPGlobalProperties();
+            NetworkInterface[] nics = NetworkInterface.GetAllNetworkInterfaces();
+            Console.WriteLine("Interface information for {0}",
+                    computerProperties.HostName);
+            if (nics == null || nics.Length < 1)
+            {
+                Console.WriteLine("  No network interfaces found.");
+                return null;
+            }
+            PhysicalAddress address;
+            Console.WriteLine("  Number of interfaces .................... : {0}", nics.Length);
+            foreach (NetworkInterface adapter in nics)
+            {
+                /* IPInterfaceProperties properties = adapter.GetIPProperties(); //  .GetIPInterfaceProperties();
+                 Console.WriteLine();
+                 Console.WriteLine(adapter.Description);
+                 Console.WriteLine(String.Empty.PadLeft(adapter.Description.Length, '='));
+                 Console.WriteLine("  Interface type .......................... : {0}", adapter.NetworkInterfaceType);*/
+                Console.Write("  Physical address ........................ : ");
+                address = adapter.GetPhysicalAddress();
+                byte[] bytes = address.GetAddressBytes();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    // Display the physical address in hexadecimal.
+                    Console.Write("{0}", bytes[i].ToString("X2"));
+                    // Insert a hyphen after each byte, unless we are at the end of the 
+                    // address.
+                    if (i != bytes.Length - 1)
+                    {
+                        Console.Write("-");
+                    }
+
+
+                }
+                Console.WriteLine();
+
+            }
+            //returns a list of all NICs(Network Interface Cards)
+            Console.Write(nics[4]);
+            return address;
+        }
+        /*public static NetworkInterface[] ShowNetworkInterfaces()
         {
             IPGlobalProperties computerProperties = IPGlobalProperties.GetIPGlobalProperties();
             NetworkInterface[] nics = NetworkInterface.GetAllNetworkInterfaces();
@@ -52,8 +94,9 @@ namespace Formelsamling.Authentication
                 
             }
             //returns a list of all NICs(Network Interface Cards)
+            Console.Write(nics[0]);
             return nics;
-        }
+        }*/
        
     }
 }
