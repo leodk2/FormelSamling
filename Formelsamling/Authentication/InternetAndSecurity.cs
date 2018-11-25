0fffa603cdcbe205c4ec2a1e026f617a53da8edb
Leo Rønsholt Mikkelsen <leox0204@edu.nextkbh.dk>
2018-11-25 20:12:24 +0100

    Changed the GetMacAddress method to return a physicaladdress intead of a NetworkInterface array

--- Formelsamling/Authentication/InternetAndSecurity.cs
+++ Formelsamling/Authentication/InternetAndSecurity.cs
@@ -80,34 +80,36 @@ namespace Formelsamling.Authentication
             string rtnStr = str.Insert(0, "'") + "'";
             return rtnStr;
         }
-        public static void AddUser(string email, NetworkInterface[] interfaces)
+        public static void AddUser(string email, PhysicalAddress[] interfaces)
         {
             sqlConnection.Open();
-            Print(interfaces[0]);
-            try
-            {
-                SQL sql = new SQL();
-                email = sql.AddGnyph(email);
-                //string interfacesStr = sql.AddGnyph(interfaces);
-                Console.WriteLine("|-|-|-|-|-|-|-|-|-|-|-|-| " + sql.AddGnyph(interfaces));
-                string macString =
+            Print(interfaces);
+            SQL sql = new SQL();
+            email = sql.AddGnyph(email);
+            //string interfacesStr = sql.AddGnyph(interfaces);
+            //Console.WriteLine("|-|-|-|-|-|-|-|-|-|-|-|-| " + sql.AddGnyph(interfaces.ToString()));
+            string macString;/* =
                     (
                     from nic in NetworkInterface.GetAllNetworkInterfaces()
                     where nic.OperationalStatus == OperationalStatus.Up
                     select nic.GetPhysicalAddress().ToString()
-                    ).FirstOrDefault();
-                macString = sql.AddGnyph(macString);
-                Console.WriteLine("|+|+|+|+|+|+|+|+|+|+|+|+| " + macString); 
+                    ).FirstOrDefault();*/
+                             //macString = sql.AddGnyph(macString);
+                             //Console.WriteLine("|+|+|+|+|+|+|+|+|+|+|+|+| " + macString); 
 
-                var arrayIndexStr = sql.AddGnyph(interfaces[0].ToString());
-                //string finalString = email + "," + interfacesStr;
-                string finalString = email + "," + macString;
-                cmd = new SqlCommand("insert into dbo.user_Codes(Uid, NoOfMacs) Values (" + finalString + ")", sqlConnection);
-                AddMac(interfaces, sqlConnection);
-                cmd.ExecuteNonQuery();
-                sqlConnection.Close();
-                //Vi kan ikke lave en kommando som tilføjer alle tre værdier på en gang, vi får en fejl hvis vi gør
-                //og vi kan heller ikke lave en ny sql command
+            var arrayIndexStr = sql.AddGnyph(interfaces.ToString());
+            //string finalString = email + "," + interfacesStr;
+            string interfacer = sql.AddGnyph(interfaces[0].ToString());
+            string finalString = email + "," + interfacer;
+            cmd = new SqlCommand("insert into dbo.user_Codes(Uid, NoOfMacs) Values (" + finalString + ")", sqlConnection);
+            //AddMac(interfaces, sqlConnection);
+            cmd.ExecuteNonQuery();
+            sqlConnection.Close();
+            //Vi kan ikke lave en kommando som tilføjer alle tre værdier på en gang, vi får en fejl hvis vi gør
+            //og vi kan heller ikke lave en ny sql command
+            /*try
+            {
+                
             }
             catch (Exception e)
             {
@@ -115,7 +117,7 @@ namespace Formelsamling.Authentication
                 Print("false exception");
                 sqlConnection.Close();
                 return;
-            }
+            }*/
             
             
         }
--- Formelsamling/Authentication/MacAddress.cs
+++ Formelsamling/Authentication/MacAddress.cs
@@ -11,7 +11,7 @@ namespace Formelsamling.Authentication
 {
     class GetMacAddress
     {
-        public static PhysicalAddress ShowNetworkInterfaces()
+        public static PhysicalAddress[] ShowNetworkInterfaces()
         {
             IPGlobalProperties computerProperties = IPGlobalProperties.GetIPGlobalProperties();
             NetworkInterface[] nics = NetworkInterface.GetAllNetworkInterfaces();
@@ -23,6 +23,7 @@ namespace Formelsamling.Authentication
                 return null;
             }
             PhysicalAddress address;
+            PhysicalAddress[] physicalAddresses;
             Console.WriteLine("  Number of interfaces .................... : {0}", nics.Length);
             foreach (NetworkInterface adapter in nics)
             {
@@ -33,6 +34,7 @@ namespace Formelsamling.Authentication
                  Console.WriteLine("  Interface type .......................... : {0}", adapter.NetworkInterfaceType);*/
                 Console.Write("  Physical address ........................ : ");
                 address = adapter.GetPhysicalAddress();
+                physicalAddresses = new PhysicalAddress[] { address };
                 byte[] bytes = address.GetAddressBytes();
                 for (int i = 0; i < bytes.Length; i++)
                 {
@@ -49,10 +51,11 @@ namespace Formelsamling.Authentication
                 }
                 Console.WriteLine();
 
+                return physicalAddresses;
             }
             //returns a list of all NICs(Network Interface Cards)
             Console.Write(nics[4]);
-            return address;
+            return null;
         }
         /*public static NetworkInterface[] ShowNetworkInterfaces()
         {
