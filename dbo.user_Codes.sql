b804517c2f49c97dd0a989daabda7ab08aaee1ca
Leo Rønsholt Mikkelsen <leomikkelsen9@gmail.com>
2018-11-25 14:20:13 +0100

    Ignore

---
+++ dbo.user_Codes.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[user_Codes] (
+    [UId]      VARCHAR (50)  NULL,
+    [Code1]    VARCHAR (50)  NULL,
+    [Mac]      VARCHAR (1000) NULL,
+    [NoOfMacs] INT           NULL,
+    [Code2]    VARCHAR (256) NULL
+);
+
