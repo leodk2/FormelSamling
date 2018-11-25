05015ac731d0b3a941ebfacaf6fc9fc9c45ea43e
Leo Rønsholt Mikkelsen <leomikkelsen9@gmail.com>
2018-11-25 14:25:05 +0100

    Changed the method for getting macadress to return ONE address

--- dbo.user_Codes.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[user_Codes] (
-    [UId]      VARCHAR (50)  NULL,
-    [Code1]    VARCHAR (50)  NULL,
-    [Mac]      VARCHAR (1000) NULL,
-    [NoOfMacs] INT           NULL,
-    [Code2]    VARCHAR (256) NULL
-);
-
